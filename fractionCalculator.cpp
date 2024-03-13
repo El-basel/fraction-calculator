3c1ed33f317e525e221c1e295b75d1423c09c705
El-basel <88934946+El-basel@users.noreply.github.com>
2024-03-13 06:07:59 +0200

    fixed input validation

--- fractionCalculator.cpp
+++ fractionCalculator.cpp
@@ -2,89 +2,34 @@
 #include <regex>
 #include <iterator>
 #include <cctype>
-double multiplication(double num1, double num2)
-{
-    double answer = num1 * num2;
-    return answer;
-}
-double division(double num1, double num2)
-{
-    double answer = num1 / num2;
-    return answer;
-}
-double difference(double num1, double num2)
-{
-    double answer = num1 - num2;
-    return answer;
-}
-double addition(double num1, double num2)
-{
-    double answer = num1 + num2;
-    return answer;
-}
-double fraction_to_double(std::string fraction)
-{
-    int numerataor = 0, denominator = 1;
-    // variable n is used to index if a number been added to numerator or not
-    int n = 1, number = 0;
-    for (int i = 0; i < fraction.size(); i++)
-    {
-        if (fraction[i] == '/' || (n == 1 && i < fraction.size() - 1))
-        {
-            numerataor += number;
-            n++;
-        }
-        else if (i < fraction.size() - 1 && n == 2)
-        {
-            denominator += number;
-        }
-        else number += int(fraction[i]);
-    }
-    if (denominator != 0) 
-    {
-        double num = double(numerataor) / denominator;
-        return num;
-    }
-    else return -1;
-}
-void operations(std::string fraction1, std::string fraction2, std::string operation)
-{
-    double num1 = fraction_to_double(fraction1);
-    double num2 = fraction_to_double(fraction2);
-    double ans;
-    if (num1 == -1 || num2 == -1)
-    {
-        std::cout << "\adenominator can't equal zero";
-        return;
-    }
-    else if (operation == "+") ans = addition(num1, num2);
-    else if (operation == "-") ans = difference(num1, num2);
-    else if (operation == "*") ans = multiplication(num1, num2);
-    else if (operation == "/") ans = division(num1, num2);
-}
+
+#include <iostream>
+#include <regex>
+#include <iterator>
+
 int getNumber(std::string input, std::string& fraction1, std::string& fraction2, std::string& operation)
 {
     std::smatch match{};
     input.push_back(' ');
-    std::string originalInput{input};
+    std::string originalInput{ input };
     std::regex fractionPattern("([-]?[0-9]+/[-]?[0-9]+|[-]?[0-9]+)");
     std::regex operationPatter("\\s[/+*-]\\s");
-    std::regex nmrtrWithoutDeno{"[-]?[0-9]+/[-]?[^-0-9]"};
+    std::regex nmrtrWithoutDeno{ "[-]?[0-9]+/[-]?[^-0-9]" };
 
-    while(std::regex_search(input,match,fractionPattern))
+    while (std::regex_search(input, match, fractionPattern))
     {
-        if(std::regex_search(input,nmrtrWithoutDeno))
+        if (std::regex_search(input, nmrtrWithoutDeno))
         {
             std::cout << "Invalid input, numerator without denominator\n";
             return -1;
         }
-        if(fraction1.length() == 0)
+        if (fraction1.length() == 0)
         {
             fraction1 = match[0];
             input = match.suffix();
             continue;
         }
-        if(fraction1.length() != 0)
+        if (fraction1.length() != 0)
         {
             fraction2 = match[0];
             input = match.suffix();
@@ -93,7 +38,7 @@ int getNumber(std::string input, std::string& fraction1, std::string& fraction2,
     }
     match = {};
     input = originalInput;
-    if(std::regex_search(input,match,operationPatter))
+    if (std::regex_search(input, match, operationPatter))
     {
         operation = match[0];
     }
@@ -119,14 +64,14 @@ int getNumber(std::string input, std::string& fraction1, std::string& fraction2,
 
 int validateInput(std::string input)
 {
-    std::regex exit{"^exit[ ]*", std::regex::icase};
+    std::regex exit{ "^exit[ ]*", std::regex::icase };
     std::smatch match{};
-    if(std::regex_match(input,match,exit))
+    if (std::regex_match(input, match, exit))
     {
         return 0;
     }
-    std::regex reg{"[^[-]?0-9\\s/+*-]+"};
-    if(std::regex_search(input,match,reg))
+    std::regex reg{"[^-?0-9\\s/+*-]+" };
+    if (std::regex_search(input, match, reg))
     {
         std::cout << "Please enter a valid input\n";
         std::cout << "Input should contain two rational numbers only ";
@@ -134,14 +79,14 @@ int validateInput(std::string input)
         std::cout << "or enter the word \"exit\" only to exit the program\n";
         return -1;
     }
-    if(!std::regex_search(input,std::regex("^([-]?[0-9]+)")))
+    if (!std::regex_search(input, std::regex("^([-]?[0-9]+)")))
     {
         std::cout << "Please enter a valid input\n";
         std::cout << "Input should start with a number\n";
         return -1;
     }
-    std::regex operations{"([/+*]{2,}|[-]{2,})"};
-    if(std::regex_search(input,match,operations))
+    std::regex operations{ "([/+*]{2,}|[-]{2,})" };
+    if (std::regex_search(input, match, operations))
     {
         std::cout << "Please enter a valid input\n";
         std::cout << match.prefix() << '[' << match.str() << ']' << match.suffix();
@@ -161,21 +106,20 @@ int main()
     while (true)
     {
         std::cout << "Please enter a rational number operations (or exit)\n";
-        std::getline(std::cin , input);
+        std::getline(std::cin, input);
 
         switch (validateInput(input)) {
             case -1:
                 continue;
             case 1:
-                if(getNumber(input,fraction1,fraction2,operation) == -1)
+                if (getNumber(input, fraction1, fraction2, operation) == -1)
                 {
                     fraction1 = fraction2 = "";
                     continue;
                 }
                 std::cout << input << '\n';
-                std::cout << fraction1 << ' ' << fraction2 << ' ' << operation;
-                std::cout << ' ' << operation.length();
-                operations(fraction1, fraction2, operation);
+                std::cout << fraction1 << ' ' << fraction2 << ' ' << operation << '\n';
+
                 break;
             case 0:
                 std::cout << "good bye\n";
@@ -184,3 +128,4 @@ int main()
         break;
     }
 }
+
