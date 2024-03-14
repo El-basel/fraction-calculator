bd2aad04f2ba4f4616aae6e810e81e514665d122
El-basel <88934946+El-basel@users.noreply.github.com>
2024-03-15 00:26:20 +0200

    added coments and part of the file header

--- fractionCalculator.cpp
+++ fractionCalculator.cpp
@@ -1,3 +1,10 @@
+// File: CS112_A2_T3_S1_20230376_20230517_20230278
+// Purpose: A calculator that can take whole integers of fractions and do operations on them
+// Author: Mahmoud Mohamed El-Basel,
+// Emails: modyelbasel50@gmail.com,
+// ID1: 20230376, Did the input validation and the extraction of the numbers and the operation
+// ID2: 20230517,
+// ID3: 20230278,
 #include <iostream>
 #include <regex>
 #include <iterator>
@@ -102,15 +109,23 @@ void operations(std::string& fraction1, std::string& fraction2, std::string& ope
     simplify(ans_numerator, ans_denominator);
 }    
 
+// here we reached that the input contains the valid digits and symbol we want and now extract them
 int getNumber(std::string input, std::string& fraction1, std::string& fraction2, std::string& operation)
 {
     std::smatch match{};
     input.push_back(' ');
+    // as we change the input when we match a pattern to continue searching the string
+    // we will need the old input also so 'originalInput' is used to save the user input
     std::string originalInput{ input };
+    // fractionPattern searches for any fraction that might contain negative number
+    // or search for a whole integer
     std::regex fractionPattern("([-]?[0-9]+/[-]?[0-9]+|[-]?[0-9]+)");
-    std::regex operationPatter("\\s[/+*-]\\s");
+    // operationPattern searches for the operation that the user inputted
+    std::regex operationPattern("\\s[/+*-]\\s");
+    // nmrtrWithoutDeno checks if the user entered a numerator and put the fraction symbol
+    // and did not provide a denominator
     std::regex nmrtrWithoutDeno{ "[-]?[0-9]+/[-]?[^-0-9]" };
-
+    int numberOfOperands{0};
     while (std::regex_search(input, match, fractionPattern))
     {
         if (std::regex_search(input, nmrtrWithoutDeno))
@@ -122,18 +137,41 @@ int getNumber(std::string input, std::string& fraction1, std::string& fraction2,
         {
             fraction1 = match[0];
             input = match.suffix();
+            ++numberOfOperands;
             continue;
         }
         if (fraction1.length() != 0)
         {
             fraction2 = match[0];
             input = match.suffix();
+            ++numberOfOperands;
             continue;
         }
     }
+    // check if the user inputted more than two operands
+    if(numberOfOperands > 2)
+    {
+        std::cout << "please enter two operands only and one operation\n";
+        return -1;
+    }
     match = {};
     input = originalInput;
-    if (std::regex_search(input, match, operationPatter))
+    // check if the input contains more than one operation
+    int numberOfOperations{0};
+    while (std::regex_search(input, match,operationPattern))
+    {
+        ++numberOfOperations;
+        input = match.suffix();
+    }
+    if(numberOfOperations > 1)
+    {
+        std::cout << "please enter two operands only and one operation\n";
+        return -1;
+    }
+    // restore the old input and start to search again for the operation
+    // after making sure that there is only one operation
+    input = originalInput;
+    if (std::regex_search(input, match, operationPattern))
     {
         operation = match[0];
     }
@@ -143,28 +181,26 @@ int getNumber(std::string input, std::string& fraction1, std::string& fraction2,
         std::cout << "or you did not enter an operation\n";
         return -1;
     }
+    // when we get the operation sequence it gets a space before it and a space after it
+    // so select the second element which is the desired operation
     operation = operation[1];
     return 0;
 
 }
 
-//std::string removeSpaces(std::string input)
-//{
-//    std::regex removeSpace{"\\s"};
-//    std::string result{};
-//    std::regex_replace(std::back_inserter(result),input.begin(),input.end(),removeSpace,"");
-//    std::cout << result << '\n';
-//    return result;
-//}
-
+// check if the input contain "exit" only or any bad input
 int validateInput(std::string input)
 {
+    // search for the patter "exit" that starts in the input and make it case-insensitive
+    // and doesn't contain any other words or digits
     std::regex exit{ "^exit[ ]*", std::regex::icase };
     std::smatch match{};
     if (std::regex_match(input, match, exit))
     {
         return 0;
     }
+    // search for any sequence in the input that contains anything not a digit
+    // or not one of the following symbols (*, +, /, -)
     std::regex reg{"[^-?0-9\\s/+*-]+" };
     if (std::regex_search(input, match, reg))
     {
@@ -174,12 +210,15 @@ int validateInput(std::string input)
         std::cout << "or enter the word \"exit\" only to exit the program\n";
         return -1;
     }
+    // check that the input starts with a number and not an arbitrary symbol from the allowed
     if (!std::regex_search(input, std::regex("^([-]?[0-9]+)")))
     {
         std::cout << "Please enter a valid input\n";
         std::cout << "Input should start with a number\n";
         return -1;
     }
+    // check that the input doesn't contain repeated symbols one after the other
+    // as it doesn't have a meaning
     std::regex operations{ "([/+*]{2,}|[-]{2,})" };
     if (std::regex_search(input, match, operations))
     {
@@ -198,9 +237,12 @@ int main()
     std::string fraction2{};
     std::string operation{};
     std::string input{};
+    std::cout << "----------------------------------\n";
+    std::cout << "| Welcome to Fraction Calculator |\n";
+    std::cout << "----------------------------------";
     while (true)
     {
-        std::cout << "Please enter a rational number operations (or exit)\n";
+        std::cout << "\nPlease enter a rational number operations (or exit)\n";
         std::getline(std::cin, input);
 
         switch (validateInput(input)) {
@@ -212,10 +254,9 @@ int main()
                     fraction1 = fraction2 = "";
                     continue;
                 }
-                //std::cout << input << '\n';
-                //std::cout << fraction1 << ' ' << fraction2 << ' ' << operation << '\n';
                 operations(fraction1, fraction2, operation);
-                break;
+                fraction2 = fraction1 = "";
+                continue;
             case 0:
                 std::cout << "good bye\n";
                 return 0;
